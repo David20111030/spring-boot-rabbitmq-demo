b0e60e953ce1c06c0cc044ea920477d84662163d
smile <2530510290@qq.com>
2018-02-05 14:11:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RabbitMQ 实战 系列文章对应demo
+
+[文章地址](https://www.nextyu.com/RabbitMQ%20in%20Action.html)
---
+++ hello-world/src/main/java/com/nextyu/rabbitmq/ProducerController.java
@@ -0,0 +1,28 @@
+package com.nextyu.rabbitmq;
+
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.lang.Console;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProducerController {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/sendMessage")
+    public Object sendMessage() {
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                String value = new DateTime().toString("yyyy-MM-dd HH:mm:ss");
+                Console.log("send message {}", value);
+                rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY, value);
+            }
+        }).start();
+        return "ok";
+    }
+
+}
